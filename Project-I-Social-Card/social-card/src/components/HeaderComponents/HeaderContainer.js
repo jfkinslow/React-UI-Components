d548d79591dffd1e8afe3223cdc80e8eb682c910
Joshua Kinslow <killajk2201@gmail.com>
2019-05-08 07:49:37 -0500

    Finished Card Header
    Finished up with the card header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderContent from "./HeaderContent.js";
+import ImageThumbnail from "./ImageThumbnail.js";
+
+export default class HeaderContainer extends React.Component {
+  render() {
+    return (
+      <div className="card-header">
+        <ImageThumbnail
+          src="http://3.bp.blogspot.com/_rbeqbj-n1Z0/SQ9j6sjTeXI/AAAAAAAAAJk/o2US22AdaM4/s1600/usertile8.bmp"
+          alt="Avatar Image"
+          className="avatar-image"
+        />
+        <HeaderContent />
+      </div>
+    );
+  }
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,20 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderTitle from "./HeaderTitle.js";
+import HeaderBody from "./HeaderBody.js";
+
+export default class HeaderContent extends React.Component {
+  render() {
+    return (
+      <div className="header-content">
+        <HeaderTitle>Lambda School @LambdaSchool 26 Jan</HeaderTitle>
+        <HeaderBody>
+          Let's learn React by building simple interfaces with components. Don't
+          try to overthink it, just keep it simple and have fun. Once you feel
+          comfortable using components you are well on your way to mastering
+          React!
+        </HeaderBody>
+      </div>
+    );
+  }
+}
